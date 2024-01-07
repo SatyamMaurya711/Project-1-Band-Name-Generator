1f50cc16c6004de16f9b21399112d3ed64bdf2f5
SATYAMMAURYA5 <20149656-SATYAMMAURYA5@users.noreply.replit.com>
2024-01-07 09:59:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+
+# Band name generator project
+
+print("Welcome to Band Name Generator Project\n")
+user_city = input("Which city did you grew up?\n")
+user_pet = input("Whats your pet name?\n")
+print("Your Band Name Is " + user_city + " " + user_pet)
